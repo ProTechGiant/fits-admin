755c9b9b18fb812cee54a077ce64d6aedaecb6b4
BilalMahmood012 <mbilalshah44@gmail.com>
2022-05-10 14:16:04 +0500

    latest-code

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,3 +1,4 @@
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://fitness-server-app.herokuapp.com";
+// export const baseUrl = "http://localhost:5000";
 // export const baseUrl = "https://fits-server.ultradone.com";
 // export const baseUrl = "https://food-server11.herokuapp.com";
