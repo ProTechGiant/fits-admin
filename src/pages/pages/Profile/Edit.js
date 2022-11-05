df1af34de444ad6531cb9d30f0c61c524fba8e7e
qaziahmedali <hqazi1731@gmail.com>
2022-11-05 12:01:55 +0500

    #comment update admin-panel

--- src/pages/pages/Profile/Edit.js
+++ src/pages/pages/Profile/Edit.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
 function Edit(props) {
-  const { Row, show, handleClose } = props;
+  const { show, handleClose } = props;
   return (
     <>
       <Modal
