c06cd708b194d85a5df22bcef533dad9eadede80
saqlain <saqlainm328@gmail.com>
2023-03-08 17:27:24 +0500

    reset baseUrl

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,4 +1,2 @@
 // export const baseUrl = "http://localhost:5000";
-export const baseUrl = "https://rhu9p94qvd.us-east-1.awsapprunner.com"; //@latest
-// export const baseUrl = "https://fits-api-server.herokuapp.com";
-// export const baseUrl = "https://food-server11.herokuapp.com";
+export const baseUrl = "https://sbmtekpvir.us-east-2.awsapprunner.com/"; //@latest
