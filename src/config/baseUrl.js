fd9142327025018de06e77b9741cef4664fc84b3
saqlain <saqlainm328@gmail.com>
2023-03-08 17:27:52 +0500

    reset baseUrl

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://localhost:5000";
-export const baseUrl = "https://sbmtekpvir.us-east-2.awsapprunner.com/"; //@latest
+export const baseUrl = "https://sbmtekpvir.us-east-2.awsapprunner.com"; //@latest
