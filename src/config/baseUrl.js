72dc25569204a124db78d3f5395c658042f80bb0
hamzaameen519 <ameenhamza392@gmail.com>
2023-03-11 13:33:51 +0500

    #comment add url

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://fits-backend-omega.vercel.app/";
+export const baseUrl = "https://fits-backend-omega.vercel.app";
 // export const baseUrl = "https://rhu9p94qvd.us-east-1.awsapprunner.com"; //@latest
 // export const baseUrl = "https://fits-api-server.herokuapp.com";
 // export const baseUrl = "https://food-server11.herokuapp.com";
